593b0cf4356d5d02d025bedffb32a80d3b3ff075
Katie Truong <katietruong.ucdavis@gmail.com>
2018-07-26 15:37:09 -0700

    Update icons

--- client/src/components/DynamicAddToCart.jsx
+++ client/src/components/DynamicAddToCart.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const DynamicAddToCart = ({onMinusSignClicked, onPlusSignClicked, itemNum}) => (
   <div className="dynamicAddToCart">
-    <div className="left" onClick={onMinusSignClicked}>Remove</div>
+    <div className="left" onClick={onMinusSignClicked}>
+      <i class="material-icons md-12">remove_circle_outline</i>
+    </div>
     <div className="center">{itemNum} in cart</div>
-    <div className="right" onClick={onPlusSignClicked}>Add</div>
+    <div className="right" onClick={onPlusSignClicked}>
+      <i class="material-icons md-12">add_circle_outline</i>
+    </div>
   </div>
 )
 
