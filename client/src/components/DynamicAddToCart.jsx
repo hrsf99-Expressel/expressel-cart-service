79af2f2cd7fdb9852f85474b8ed00f2641217abb
Katie Truong <katietruong.ucdavis@gmail.com>
2018-07-26 11:59:15 -0700

    Refactor button components from stateful to stateless

--- client/src/components/DynamicAddToCart.jsx
+++ client/src/components/DynamicAddToCart.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
-class DynamicAddToCart extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="dynamicAddToCart">
-        <div className="left" onClick={this.props.onMinusSignClicked}>Remove</div>
-        <div className="center">{this.props.itemNum} in cart</div>
-        <div className="right" onClick={this.props.onPlusSignClicked}>Add</div>
-      </div>
-    )
-  }
-};
+const DynamicAddToCart = ({onMinusSignClicked, onPlusSignClicked, itemNum}) => (
+  <div className="dynamicAddToCart">
+    <div className="left" onClick={onMinusSignClicked}>Remove</div>
+    <div className="center">{itemNum} in cart</div>
+    <div className="right" onClick={onPlusSignClicked}>Add</div>
+  </div>
+)
 
 export default DynamicAddToCart;
